49e3c41780a1f3b9cc2ece9c3fd128f4bfec676e
patelsuraj3011 <patelsuraj301@gmail.com>
2022-12-11 13:58:38 +0530

    Created checkSubscription

--- routes/renewal.js
+++ routes/renewal.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const payment = require("../controller/renewal/payment");
 const selectBatch = require("../controller/renewal/selectBatch");
+const checkSubscription = require("../controller/renewal/checkSubscription");
 
 
 router.post("/", authenticate, payment);
 
 router.post("/selectBatch", authenticate, selectBatch);
+
+router.get("/checkSubscription", authenticate, checkSubscription);
 
 module.exports = router;
