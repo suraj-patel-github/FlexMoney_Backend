1ac81015b16ab1896d5254a84b8523a0a18bde0c
patelsuraj3011 <patelsuraj301@gmail.com>
2022-12-12 13:33:07 +0530

    added batches.js

---
+++ controller/batch/getBatch.js
@@ -0,0 +1,21 @@
+const pool = require("../../config/db") 
+
+const getBatch = async(req, res) =>{
+    try{
+
+        const batch_id = req.params.batchId;
+        
+        const query = `select * from batches where id = $1`;
+
+        const batch = await pool.query( query, [batch_id] );
+        
+        return res.status(200).json({message : "Batch found", batch: batch.rows[0]});
+
+    }
+    catch(err){
+        return res.status(500).json({message :"Server Error"});
+    }
+} 
+
+
+module.exports = getBatch;
--- routes/batches.js
+++ routes/batches.js
@@ -1,10 +1,13 @@
 
 
+const getBatch = require("../controller/batch/getBatch");
 const getBatches = require("../controller/batch/getBatches");
 const authenticate = require("../utils/authenticate");
 
 const router = require("express").Router();
 
 router.get("/", authenticate, getBatches);
+
+router.get("/single/:batchId", authenticate, getBatch);
 
 module.exports = router;
